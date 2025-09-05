a2add955944f161b09e7aaf51f5c8a37a7c1fc5d
Ryan Huynh <ryanhuynh1803@gmail.com>
2025-09-05 20:33:43 +0700

    [dyad] Removed 'Lịch sử & Báo cáo' tab - wrote 1 file(s)

--- src/components/profile/TestAndHistoryView.tsx
+++ src/components/profile/TestAndHistoryView.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import DoTestView from './DoTestView';
-import HistoryHubView from './HistoryHubView';
+// HistoryHubView không còn được sử dụng trực tiếp ở đây nữa
 
 const TestAndHistoryView = () => {
   return (
     <div className="p-4 sm:p-6">
-      <Tabs defaultValue="do-test" className="w-full">
-        <TabsList className="grid w-full grid-cols-2 mb-6">
-          <TabsTrigger value="do-test">Làm Bài Test</TabsTrigger>
-          <TabsTrigger value="history-report">Lịch sử & Báo cáo</TabsTrigger>
-        </TabsList>
-        <TabsContent value="do-test">
-          <DoTestView />
-        </TabsContent>
-        <TabsContent value="history-report">
-          <HistoryHubView />
-        </TabsContent>
-      </Tabs>
+      {/* Loại bỏ Tabs và chỉ hiển thị DoTestView */}
+      <DoTestView />
     </div>
   );
 };
