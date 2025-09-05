00e3ea9010fa6ee404f1d846bfd46998d75f687f
Ryan Huynh <ryanhuynh1803@gmail.com>
2025-09-05 21:27:40 +0700

    Reverted all changes back to version cc597853a2af985320abd1742621d0c023395fe8

--- src/components/landing/TestimonialSection.tsx
+++ src/components/landing/TestimonialSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// Removed Carousel imports as we are switching to a grid layout
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import TestimonialCard from './TestimonialCard';
 import { testimonialData } from '@/data/testimonialData';
 
@@ -15,14 +15,20 @@ const TestimonialSection = () => {
             </p>
         </div>
 
-        {/* Vùng chứa grid cho lời chứng thực */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {/* Vùng chứa slider */}
+        <Carousel opts={{ align: "start" }} className="w-full">
+          <CarouselContent className="-ml-4">
             {testimonialData.map((testimonial, index) => (
-              <div key={index} className="h-full">
-                <TestimonialCard testimonial={testimonial} />
-              </div>
+              <CarouselItem key={index} className="pl-4 md:basis-1/2 lg:basis-1/3">
+                <div className="p-1 h-full">
+                  <TestimonialCard testimonial={testimonial} />
+                </div>
+              </CarouselItem>
             ))}
-        </div>
+          </CarouselContent>
+          <CarouselPrevious className="hidden sm:flex" />
+          <CarouselNext className="hidden sm:flex" />
+        </Carousel>
       </div>
     </section>
   );
